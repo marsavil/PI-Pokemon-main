f2b0f6e1562c1d3068dce18f829226d0d49a1ff1
Mariano Villaescusa <msvillaescusa@gmail.com>
2022-12-16 21:23:33 -0300

    models and db

--- PI-Pokemon-main/api/src/db.js
+++ PI-Pokemon-main/api/src/db.js
@@ -2,14 +2,40 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const {
-  DB_USER, DB_PASSWORD, DB_HOST,
-} = process.env;
+const {DB_USER, DB_PASSWORD, DB_HOST} = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/pokemon`, {
-  logging: false, // set to console.log to see the raw SQL queries
-  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-});
+// const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/pokemon`, {
+//   logging: false, // set to console.log to see the raw SQL queries
+//   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+// });
+
+let sequelize = 
+  process.env.NODE_ENV === "production"
+    ? new Sequelize({
+      database: DB_NAME,
+      dialect: "postgres",
+      host: DB_HOST,
+      port: 5432,
+      username: DB_USER,
+      password: DB_PASSWORD,
+      pool: {
+        max: 3,
+        min: 1,
+        idle: 10000,
+      },
+      dialectOptions:{
+        ssl:{
+          require: true,
+          rejectUnauthorized: false,
+        },
+        keepAlive: true
+      },
+      ssl: true,
+    })
+    : new Sequelize(
+      `postgres://${DB_USER}: ${DB_PASSWORD}@${DB_HOST}/pokemon`,
+      { logging: false, native: false }
+    );
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
@@ -30,10 +56,12 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Pokemon } = sequelize.models;
+const { Pokemon, Type } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
+Pokemon.belongsToMany(Type, {through:'pokemon_type'})
+Type.belongsToMany(Pokemon, {through:'pokemon_type'})
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
--- PI-Pokemon-main/api/src/models/Pokemon.js
+++ PI-Pokemon-main/api/src/models/Pokemon.js
@@ -4,9 +4,73 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('pokemon', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+      unique: true,
+      validate: {
+        len: [0, 20]
+      }
     },
+    hp: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+      validate: {
+        min: 0,
+        max: 999
+      }
+    },
+    attack: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+      validate: {
+        min: 0,
+        max: 999
+      }
+    },
+    defense: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+      validate: {
+        min: 0,
+        max: 999
+      }
+    },
+    speed: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+      validate: {
+        min: 0,
+        max: 999
+      }
+    },
+    height:{
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+      validate: {
+        min: 0,
+        max: 999
+      }
+    },
+    weight:{
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+      validate: {
+        min: 0,
+        max: 9999
+      }
+    },
+    image: {
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    timestamps: false,
   });
 };
