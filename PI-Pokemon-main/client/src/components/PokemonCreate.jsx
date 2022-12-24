e28823f217e0a5751f0b1eb24414e8ea189b1878
Mariano Villaescusa <msvillaescusa@gmail.com>
2022-12-24 09:46:40 -0300

    createPokemon Validation - not working well yet

--- PI-Pokemon-main/client/src/components/PokemonCreate.jsx
+++ PI-Pokemon-main/client/src/components/PokemonCreate.jsx
@@ -3,10 +3,34 @@ import { Link, useHistory } from "react-router-dom";
 import { postPokemon, getTypes } from "../actions";
 import { useDispatch, useSelector } from "react-redux";
 
+function validate(input){
+  let errors = {};
+  if (input.name === ''){
+    errors.name = 'A name is required';
+  }else if (input.image === ''){
+    errors.image = 'You must provide an image url'
+  }else if (input.hp === '' || input.hp < 0 || input.hp > 999){
+    errors.hp = 'You must enter a valid number from 0 to 999'
+  }else if (input.attack === '' || Number(input.attack) < 0 || Number(input.attack) > 999){
+    errors.attack = 'You must enter a valid number from 0 to 999'
+  }else if (input.defense === '' || Number(input.defense) < 0 || Number(input.defense) > 999){
+    errors.defense = 'You must enter a valid number from 0 to 999'
+  }else if (input.speed === '' || input.speed < 0 || input.speed > 999){
+    errors.speed = 'You must enter a valid number from 0 to 999'
+  }else if (input.weight === '' || Number(input.weight) < 0 || Number(input.weight) > 999){
+    errors.weight = 'You must enter a valid number from 0 to 999'
+  }else if (input.height === '' || Number(input.height) < 0 || Number(input.height) > 999){
+    errors.height = 'You must enter a valid number from 0 to 999'
+  }else if (input.types === []){
+    errors.types = 'You must select, at least, one valid pokemon type'
+  }
+  return errors;
+}
 export default function PokemonCreate() {
   const dispatch = useDispatch();
   const history = useHistory()
   const types = useSelector((state) => state.types);
+  const [errors, setErrors] = useState({});
   const [input, setInput] = useState({
     name: "",
     image: "",
@@ -27,7 +51,12 @@ export default function PokemonCreate() {
       ...input,
       [e.target.name]: e.target.value
     })
+    setErrors(validate({
+      input,
+      [e.target.name]: e.target.value
+    }))
     console.log(input)
+    console.log(errors)
   }
   function handleSelect(e){
     setInput({
@@ -70,45 +99,74 @@ export default function PokemonCreate() {
         <div>
           <label>Name:</label>
           <input type="text" value={input.name} name="name" onChange={(e) => handleChange(e)} />
+          {errors.name && (
+            <p className="error">{errors.name}</p>
+          )}
         </div>
         <div>
           <label>Image:</label>
           <input type="text" value={input.image} name="image" onChange={(e) => handleChange(e)}/>
+          {errors.image && (
+            <p className="error">{errors.image}</p>
+          )}
         </div>
         <div>
           <label>Hp:</label>
           <input type="text" value={input.hp} name="hp" onChange={(e) => handleChange(e)}/>
+          {errors.hp && (
+            <p className="error">{errors.hp}</p>
+          )}
         </div>
         <div>
           <label>Attack:</label>
           <input type="text" value={input.attack} name="attack" onChange={(e) => handleChange(e)}/>
+          {errors.attack && (
+            <p className="error">{errors.attack}</p>
+          )}
         </div>
         <div>
           <label>Defense:</label>
           <input type="text" value={input.defense} name="defense" onChange={(e) => handleChange(e)}/>
+          {errors.defense && (
+            <p className="error">{errors.defense}</p>
+          )}
         </div>
         <div>
           <label>Speed:</label>
           <input type="text" value={input.speed} name="speed" onChange={(e) => handleChange(e)}/>
+          {errors.speed && (
+            <p className="error">{errors.speed}</p>
+          )}
         </div>
         <div>
           <label>Height:</label>
           <input type="text" value={input.height} name="height" onChange={(e) => handleChange(e)}/>
+          {errors.height && (
+            <p className="error">{errors.height}</p>
+          )}
         </div>
         <div>
           <label>Weight:</label>
           <input type="text" value={input.weight} name="weight" onChange={(e) => handleChange(e)}/>
+          {errors.weight && (
+            <p className="error">{errors.weight}</p>
+          )}
         </div>
         <select onChange={(e) => handleSelect(e)}>
+          {errors.types && (
+            <p className="error">{errors.types}</p>
+          )}
           {types.map((t) => (
             <option value={t.name}>{t.name}</option>
           ))
           }
         </select>
         <ul><li>{input.types.map(t => t +', ')}</li></ul>
-        <button type='submit'>Create Pokemon</button>
+        {errors !== {} ? 
+        (<h4>You must fill all the fields</h4>) : 
+        (<button type='submit'>Create Pokemon</button>)
+        }
       </form>
-      {console.log(input.types)}
       {input.types.map(t => 
         <div className= 'delTypes'>
           <p>{t}</p>
